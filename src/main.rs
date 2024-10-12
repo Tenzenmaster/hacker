5b674e68ab866817e0b73435e01774366ff1f921
Tenzenmaster <160365410+Tenzenmaster@users.noreply.github.com>
2024-10-12 18:09:25 -0400

    prettier

--- src/main.rs
+++ src/main.rs
@@ -14,18 +14,36 @@ fn main() {
 
     let mut rng = rand::thread_rng();
 
+    execute!(
+        io::stdout(),
+        style::SetForegroundColor(style::Color::Blue),
+        style::Print("Hacking password...\n"),
+        style::SetForegroundColor(style::Color::Red),
+    ).unwrap();
     for i in 0..password.len() {
-        for _ in 0..rng.gen_range(0..16) {
-            let mut str = password[0..i].to_owned();
-            str.push_str(&generate(password.len() - i, ALPHANUMERIC));
+        for _ in 0..rng.gen_range(0..32) {
+            let s1 = &password[0..i];
+            let s2 =generate(password.len() - i, ALPHANUMERIC);
             execute!(
                 io::stdout(),
                 cursor::SavePosition,
-                style::Print(str),
+                style::SetForegroundColor(style::Color::Green),
+                style::Print(s1),
+                style::SetForegroundColor(style::Color::Red),
+                style::Print(s2),
                 cursor::RestorePosition,
             ).unwrap();
             thread::sleep(Duration::from_millis(50));
         }
     }
-    println!("{password}");
+
+    execute!(
+        io::stdout(),
+        style::SetForegroundColor(style::Color::Green),
+        style::SetBackgroundColor(style::Color::DarkGrey),
+        style::Print(format!("{password}\n")),
+        style::SetForegroundColor(style::Color::Blue),
+        style::SetBackgroundColor(style::Color::Reset),
+        style::Print("Password successfully hacked.\n"),
+    ).unwrap();
 }
